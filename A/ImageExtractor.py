a55222f32239fc2d1927a48938091029e6809bfb
user31415926 <kamilkowalski212@gmail.com>
2018-11-17 01:17:40 +0100

    Kontrast
    Signed-off-by: user31415926 <kamilkowalski212@gmail.com>

--- A/ImageExtractor.py
+++ A/ImageExtractor.py
@@ -2,17 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 import cv2
-def round(x):
-	x/=255
-	if x>0.75:
-		return 1
-	return 0
-def scale(x):
-	return [round(y) for y in x]
-img = cv2.imread('0_51_right_13.jpg',0)
-array = np.array(img)
-array = np.divide(array,255*1.49)
-array = np.round(array)
-print(array[0][34])
-plt.imshow(array)
-plt.show()
+def getContrast(filename):
+	img = cv2.imread(filename,0)
+	array = np.array(img)
+	array = np.divide(array,255*1.49)
+	array = np.round(array)
+	#print(array[0][34])
+	plt.imshow(array)
+	plt.show()
+	return array
+getContrast('0_56_right_18.jpg')
